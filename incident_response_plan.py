bffc00f48a170aa8982aee8a5df4f51aada3fc32
Coding with Yash <www.rahuldivedi012345@gmail.com>
2025-09-17 21:01:36 +0530

    incident_response_plan.py

---
+++ incident_response_plan.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+logging.info("Incident Response Plan initiated.")
+logging.info("Formed Incident Response Team (IRT).")
+logging.info("Deployed SIEM and EDR tools.")
+logging.info("Conducted staff training on incident response procedures.")
